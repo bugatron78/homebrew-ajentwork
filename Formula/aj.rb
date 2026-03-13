ee5390a3f5ae6fc744d8d95c3a3523b74797c552
Jonathon Kennemer <bugless@RobotDrumCoMac.local>
2026-03-13 11:34:05 -0500

    Declare Apache 2.0 license

--- Formula/aj.rb
+++ Formula/aj.rb
@@ -3,6 +3,7 @@ class Aj < Formula
   homepage "https://github.com/bugatron78/ajentwork"
   url "https://github.com/bugatron78/ajentwork/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "280f81970d3451de693cb4e037fc0e64fd124cbbf7f754efde0bc99ccd39e43a"
+  license "Apache-2.0"
 
   depends_on "go" => :build
 
