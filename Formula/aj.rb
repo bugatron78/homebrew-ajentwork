d50a837b61bd3336ee731f43bc9659de10cbb12f
Jonathon Kennemer <bugless@RobotDrumCoMac.local>
2026-03-13 19:05:14 -0500

    Update aj to v0.1.4

--- Formula/aj.rb
+++ Formula/aj.rb
@@ -1,8 +1,8 @@
 class Aj < Formula
   desc "Local-first work tracker for AI agents"
   homepage "https://github.com/bugatron78/ajentwork"
-  url "https://github.com/bugatron78/ajentwork/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "be8f3fbecd221b6fc0624b3ddd97722e0bf0b7ebb03a61a970d461be06342540"
+  url "https://github.com/bugatron78/ajentwork/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "6fad9c74696731d82b6e58a89503005885edc0d4c59ec282fa3a40276f6198fb"
   license "Apache-2.0"
 
   depends_on "go" => :build
